d116cae4b401a75a7d0721eb9d40e1a8ec3541f6
Gabor Ballabas <gbr666@gmail.com>
2024-10-15 15:04:55 +0200

    Add integration test for catalog-service

--- src/test/kotlin/com/polarbookshop/catalogservicekotlin/CatalogServiceKotlinApplicationTests.kt
+++ src/test/kotlin/com/polarbookshop/catalogservicekotlin/CatalogServiceKotlinApplicationTests.kt
@@ -1,13 +1,40 @@
 package com.polarbookshop.catalogservicekotlin
 
+import com.polarbookshop.catalogservicekotlin.domain.Book
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
-@SpringBootTest
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class CatalogServiceKotlinApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+    @Autowired
+    lateinit var webTestClient: WebTestClient
 
+    @Test
+    fun `when post request then book created`() {
+        val expectedBook = Book(
+            isbn = "1231231231",
+            title = "Title",
+            author = "Author",
+            price = 9.90,
+        )
+
+        webTestClient
+            .post()
+            .uri("/books")
+            .bodyValue(expectedBook)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(Book::class.java)
+            .value { actualBook ->
+                assertNotNull(actualBook)
+                assertEquals(expectedBook.isbn, actualBook.isbn)
+            }
+    }
 }
