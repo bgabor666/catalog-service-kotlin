382d2c2c7faf74c2608c75c33b15e3496f337f58
Gabor Ballabas <gbr666@gmail.com>
2024-10-15 09:39:36 +0200

    Add nullable Book fields validation solution in comment

--- src/main/kotlin/com/polarbookshop/catalogservicekotlin/domain/Book.kt
+++ src/main/kotlin/com/polarbookshop/catalogservicekotlin/domain/Book.kt
@@ -23,3 +23,41 @@ data class Book(
     @field:Positive(message = "The book price must be greater than zero.")
     val price: Double,
 )
+
+/*
+data class BookRequest(
+    @field:NotBlank(message = "The book ISBN must be defined.")
+    @field:Pattern(
+        regexp = "^([0-9]{10}|[0-9]{13})$",
+        message = "The ISBN format must be valid."
+    )
+    val isbn: String?,
+
+    @field:NotBlank(message = "The book title must be defined.")
+    val title: String?,
+
+    @field:NotBlank(message = "The book author must be defined.")
+    val author: String?,
+
+    @field:NotNull(message = "The book price must be defined.")
+    @field:Positive(message = "The book price must be greater than zero.")
+    val price: Double?
+)
+
+data class Book(
+    val isbn: String,
+    val title: String,
+    val author: String,
+    val price: Double
+)
+
+fun BookRequest.toBook(): Book {
+    return Book(
+        isbn = requireNotNull(this.isbn) { "ISBN must not be null" },
+        title = requireNotNull(this.title) { "Title must not be null" },
+        author = requireNotNull(this.author) { "Author must not be null" },
+        price = requireNotNull(this.price) { "Price must not be null" }
+    )
+}
+
+*/
